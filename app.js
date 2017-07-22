38ef7b8fbca5499b4051946c416adb822b3b5dbe
Harish Ramachandran <harishsr@icloud.com>
2017-07-22 11:55:45 -0400

    initialize empty events; populate with data; add new data on click

--- app.js
+++ app.js
@@ -4,12 +4,51 @@ new Vue({
 
     // any data we will need to use on the page
     data: {
-        event: {}
+        event: {
+            name: '',
+            description: '',
+            date: ''
+        },
+        events: []
     },
 
     // any function(s) in the 'ready' key will run when the app loads
-    ready: function() {},
+    ready: function() {
+        // initialize some basic data
+        this.fetchEvents();
+    },
 
     // methods we'll use in the app
-    methods: {}
+    methods: {
+        fetchEvents: function(){
+            var events = [
+                {
+                    id: 1,
+                    name: 'Jacksonville Jumbo Shrimp Game',
+                    description: 'A minor league baseball game',
+                    date: '2017-07-22'
+                },{
+                    id: 2,
+                    name: 'Birthday Party',
+                    description: "Ramsey's wife Megan is throwing him a surprise 40th birthday party",
+                    date: '2017-08-05'
+                },{
+                    id: 3,
+                    name: 'Greece Work-cation',
+                    description: 'Head to Crete, Greece with my wife for my work trip'
+                }
+            ];
+
+            // $set is a convenience method Vue gives us to push data to an
+            // array
+            this.$set('events', events);
+        },
+
+        addEvent: function() {
+            if(this.event.name) {
+                this.events.push(this.event);
+                this.event = { name: '', description: '', date: '' }
+            }
+        }
+    }
 });
