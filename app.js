bbcc9db696be2d0bb532c2984fb5f627794e025d
Harish Ramachandran <harishsr@icloud.com>
2017-07-22 11:15:16 -0400

    outline of our app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    // the element on the page this will go in: the 'events' id
+    el: '#events',
+
+    // any data we will need to use on the page
+    data: {},
+
+    // any function(s) in the 'ready' key will run when the app loads
+    ready: function() {},
+
+    // methods we'll use in the app
+    methods: {}
+});
